a8e1ac143b686ebcb311b94e7031ed54b38e31ad
candleconch <91436222+candleconch@users.noreply.github.com>
2025-08-15 19:45:09 +0000

    Complete step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import SearchFilter from './components/SearchFilter'
 import Form from './components/Form'
 import Phonebook from './components/Phonebook'
 
 const App = () => {
-   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  
+  const hook = () => {
+    console.log('effect activated');
+    axios
+      .get('/persons')
+      .then(response => {
+        console.log('fulfilled');
+        setPersons(response.data)
+      })
+      .catch(reason => {
+        debugger;
+        console.log(reason)})
+  }
+
+  useEffect(hook, []);
+
   const handleChange = (value) => {
     if (value === 'name'){
       return e => setNewName(e.target.value);
