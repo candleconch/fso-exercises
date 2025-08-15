f3449cbfdacd8be279cac09c4e4ebd06b1aa0ab1
candleconch <91436222+candleconch@users.noreply.github.com>
2025-08-15 10:44:41 +0000

    Complete step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+   const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
   const handleSubmit =(e) => {
       e.preventDefault();
     if (newName.trim().length === 0) {
       return;
     }
     const names = persons.map(p => p.name)
-    console.log(names)
     if (names.includes(newName)){
       alert(`${newName} is already added to phonebook`)
       setNewName('');
@@ -23,9 +27,19 @@ const App = () => {
     setNewName('');
     setNewNumber('');
   }
+  const shown = persons.filter(p => {
+    if (filter.trim().length > 0) {
+      return p.name.toLowerCase().includes(filter)
+    }
+    return p;
+  })
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with
+        <input value={filter} onChange={(e) => setFilter(e.target.value)}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <div>name:  
@@ -41,7 +55,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {shown.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
