992d22a08de8d1a1450ace37f116c309f2db23dc
candleconch <91436222+candleconch@users.noreply.github.com>
2025-08-31 15:43:33 +0300

    Complete ex.4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -3,8 +3,15 @@ const assert = require('assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
 
 test/*.only*/('blogs returned as json', async () => {
     await api
@@ -13,7 +20,7 @@ test/*.only*/('blogs returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test.only('blogs have id property and not _id', async () => {
+test/*.only*/('blogs have id property and not _id', async () => {
     const response = await api
                 .get('/api/blogs')
                 .expect(200)
@@ -23,6 +30,27 @@ test.only('blogs have id property and not _id', async () => {
         assert.ok('id' in item)
         assert.ok(!('_id' in item))
     }
+})
+
+test.only('a blog post can be added', async () => {
+    const newBlog = {
+        title: "No time to study",
+        author: "Anxious Pen",
+        url: "https://example.com/async-await",
+        likes: 0,
+    }
+    await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const blogsResult = await helper.blogsInDb()
+    const titles = blogsResult.map(b => b.title)
+
+    assert.strictEqual(blogsResult.length, helper.initialBlogs.length + 1)
+
+    assert(titles.includes('No time to study'))
 })
 after(async () => {
     await mongoose.connection.close();
